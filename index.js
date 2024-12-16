5cf0d31ecff10a40bceb6e71c7b0cbcda861e24c
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-12-16 16:27:36 +0200

    initialising app

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const PORT = 2001;
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`app is running on http://localhost:${PORT}`));
+// appRoute 
+app.get("/" , (req ,res)=>{
+    res.status(200).json({status:"success" , message:"app is running successfully"})
+}) 
+// the universal route 
+app.get("*" , (req , res)=>{
+    try {
+        res.status(404).json({status:"failed" , message:"route not found "})
+    } catch (error) {
+        res.status(500).json({status:"failed" , message:error.message})
+    }
+}) 
+
+

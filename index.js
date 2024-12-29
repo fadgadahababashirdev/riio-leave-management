139891db34d98ff080a2b3cac47a300e54c21734
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-12-29 19:30:34 +0200

    adding listen

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const PORT = process.env.APP_PORT || 1200
 app.use(express.json());
 app.use(morgan('combined'));
 
-app.listen(process.env.APP_PORT, () => console.log(`app is running on http://localhost:${PORT}`));
 // appRoute 
 app.get("/" , (req ,res)=>{
     res.status(200).json({status:"success" , message:"app is running successfully"})
 }) 
 app.use("/" , router)
+
 // the universal route 
 app.get("*" , (req , res)=>{
     try {
@@ -22,6 +22,9 @@ app.get("*" , (req , res)=>{
     } catch (error) {
         res.status(500).json({status:"failed" , message:error.message})
     }
-}) 
+})  
+
+
+app.listen(process.env.APP_PORT, () => console.log(`app is running on http://localhost:${PORT}`));
 
 

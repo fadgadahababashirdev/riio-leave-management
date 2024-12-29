27132e42555570e84ba46329a6596acb9641eb36
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-12-29 19:52:15 +0200

    debugging

--- controllers/login.js
+++ controllers/login.js
@@ -5,31 +5,41 @@ const jwt = require('jsonwebtoken');
 const Account = require('../models/account');
 
 const login = async (req, res) => {
+  console.log("hello this is before try")
   try {
+    
     const { email, password } = req.body;
+    console.log("hello this bellow  req")
     const user = await Account.findOne({ where: { email } });
+    console.log("hello this is bellow user")
 
     if (!user) {
       return res
         .status(404)
         .json({ status: 'Failed', message: 'email not found' });
     }
+    console.log("THis is when no user found bellow")
     const comparePassword = await bcrypt.compare(password, user.password);
+    console.log("bellow compare")
     if (!comparePassword) {
       return res
         .status(404)
         .json({ status: 'Failed ', message: 'Wrong password ,try again !' });
-    }
+    } 
+    console.log("this is bellow the cdac")
     res.status(200).json({
       status: 'success',
       message: 'user loged in successfully',
       token: jwt.sign({ userId: user.id }, process.env.secret , {
         expiresIn: '24h',
       }),
-    });
+    }); 
+
   } catch (error) {
-    console.log("the error" ,error)
-    res.status(500).json({ status: 'Failed', message: error.message });
+    console.log("the error")
+    console.log(error)
+    res.status(500).json({ status: 'Failed', message: error.message }); 
+
   }
 };
 

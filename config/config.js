43aa572bfee9b38a66e70b94afd09b62c252954e
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-12-29 10:52:03 +0200

    adding user port manually

--- config/config.js
+++ config/config.js
@@ -1,41 +1,27 @@
-require("dotenv").config();
+require("dotenv").config()
 
 module.exports = {
   development: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    port: process.env.PORT,
-    ssl: false,
+    username:process.env.DB_USERNAME ,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    host:process.env.DB_HOST,
+    dialect:'postgres',
   },
   test: {
     username: process.env.USERNAME,
-    password: process.env.PASSWORD,
+    password:process.env.PASSWORD,
     database: process.env.DATABASE,
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    port: process.env.PORT,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    host:process.env.HOST,
+    dialect:process.env.DIALECT,
+    port:5432 ,
+    
   },
   production: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    port: process.env.PORT,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'postgres',
   },
 };
--- config/database.js
+++ config/database.js
@@ -1,18 +1,6 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-
-// Get the environment (default to "development" if not set)
-const env = process.env.NODE_ENV || "development";
-
-// Load the configuration for the current environment
-const config = require("./config")[env];
-
-// Create a Sequelize instance with the appropriate configuration
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-    port: config.port,
-    dialectOptions: config.dialectOptions,
-  });
-  
-module.exports = sequelize;
+require("dotenv").config()
+const {Sequelize} = require("sequelize") 
+const env = process.env.NODE_DEVELOPMENT || "development" 
+const config = require("./config")[env]
+const sequelize = new Sequelize(config)
+module.exports = sequelize

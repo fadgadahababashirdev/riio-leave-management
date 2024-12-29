4505f2a81d1f7561cefb52d243725532cd8aaa2b
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-12-29 20:11:30 +0200

    commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const {Sequelize} = require("sequelize") 
-const env = process.env.NODE_DEVELOPMENT || "development" 
+const env = process.env.NODE_ENV || "development" 
 const config = require("./config")[env]
 const sequelize = new Sequelize(config)
 module.exports = sequelize

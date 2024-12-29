25710557e87196d5a186255206676f1c63e4b384
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-12-29 11:03:09 +0200

    unconflicting port

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const PORT = 2001;
+
 const express = require('express');
 const router = require('./routes/riioRoute');
 const app = express();
 
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`app is running on http://localhost:${PORT}`));
+app.listen(process.env.APP_PORT, () => console.log(`app is running on http://localhost:${process.env.APP_PORT}`));
 // appRoute 
 app.get("/" , (req ,res)=>{
     res.status(200).json({status:"success" , message:"app is running successfully"})

e88859e3665b6a62827635ad035657319777a8a4
fdahaba <138160871+fadgadahababashirdev@users.noreply.github.com>
2025-01-12 15:00:50 +0200

    supporting pdf files

--- helpers/multer.js
+++ helpers/multer.js
@@ -6,20 +6,20 @@ const imageStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'riio-images',
-    allowed_formats: ['jpeg', 'jpg', 'png', 'svg'],
+    allowed_formats: ['jpeg', 'jpg', 'png', 'svg' ,'pdf'],
     public_id: (req, file) => `${Date.now()}-${file.originalname}`,
   },
 });
 
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/svg'];
+  const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/svg' ,'application/pdf'];
 
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
     cb(
       new Error(
-        `Unsupported file type: ${file.mimetype}. Allowed types are jpeg, jpg, and png.`
+        `Unsupported file type: ${file.mimetype}. Allowed types are jpeg, jpg,pdf , and png.`
       )
     );
   }
